d9cd85670001b06e0e18ca70aba8401f57295b21
Jasur Amirov <ajasur@gmail.com>
2021-01-13 20:00:47 -0800

    combined js funtions into one

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var generatePasswordButton = document.getElementById("generate");
 
-var onlyUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var onlyLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var onlyUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var onlyLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var onlySpecial = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
 var onlyNumeric = [0,1,2,3,4,5,6,7,8,9];
 
@@ -30,7 +30,7 @@ function generateOptions() {
     var shouldIncludeUppercase = confirm("Do you want to include uppercase characters?");
     
     if (!shouldIncludeLowercase && !shouldIncludeUppercase && !shouldIncludeNumeric && !shouldIncludeSpecialCharacters) {
-        alert("Your password must contain at least one special, numeric, lowercase or uppercase character");
+        alert("Your password must contain at least one special, numeric, lowercase, or uppercase character");
         return;
     }
     var questionOptions = {
@@ -46,38 +46,44 @@ function generateOptions() {
 function generatePassword() {
 
     var options = generateOptions();
+    console.log(options)
 
     var passwordPool = [];
+    console.log(passwordPool)
 
     if (options.specialCharacters) {
-        for (i = 0; i < onlySpecial.length; i++) {
+        for (i = 0; i < onlySpecial.length; ++i) {
             passwordPool.push(onlySpecial[i]);
         }
     } 
     if (options.numeric) {
-        for (i = 0; i < onlyNumeric.length; i++) {
+        for (i = 0; i < onlyNumeric.length; ++i) {
         passwordPool.push(onlyNumeric[i]);
         }
     }
     if (options.lowerCase) {
-        for (i = 0; i < onlyLower.length; i++) {
+        for (i = 0; i < onlyLower.length; ++i) {
         passwordPool.push(onlyLower[i]);
         }
     }
     if (options.upperCase) {
-        for (i = 0; i < onlyUpper.length; i++) {
+        for (i = 0; i < onlyUpper.length; ++i) {
         passwordPool.push(onlyUpper[i]);
         }
     }
     var finalPassword = [];
 
-    for (let i = 0; i < options.length; i++) {
+    for (let i = 0; i < options.length; ++i) {
         var randomPicker = Math.floor(Math.random() * Math.floor(passwordPool.length));
-        finalPassword.push(passwordPool[randomPicker])
+         finalPassword.push(passwordPool[randomPicker])
     }
-    var superPassword = finalPassword.join('');
 
-    document.getElementById("password").textContent = superPassword;
+    console.log(finalPassword)
+
+    var superFinal = finalPassword.join('');
+    console.log(superFinal)
+
+    document.getElementById("password").textContent = superFinal;
 }
 
 generatePasswordButton.addEventListener("click", generatePassword);
